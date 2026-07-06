5f5d6eb775f8fac4ac165ff4c1e9419dd28847c0
solaaa383 <4624021@ed.tus.ac.jp>
2026-07-06 12:01:59 +0900

    data

--- data/data.py
+++ data/data.py
@@ -3,7 +3,7 @@ recipes = [
         "id": 1,
         "name": "肉じゃが",
         "category": "主菜",
-        "ingredients": ["じゃがいも", "玉ねぎ", "牛肉", "にんじん"]
+        "ingredients": ["じゃがいも", "玉ねぎ", "牛肉", "にんじん","白滝"],
     },
     {
         "id": 2,
@@ -15,13 +15,13 @@ recipes = [
         "id": 3,
         "name": "しょうが焼き",
         "category": "主菜",
-        "ingredients": ["豚肉", "玉ねぎ"]
+        "ingredients": ["豚ロース", "玉ねぎ"]
     },
     {
         "id": 4,
         "name": "ロールキャベツ",
         "category": "主菜",
-        "ingredients": ["キャベツ", "ひき肉", "玉ねぎ", "パン粉", "牛乳", "卵"]
+        "ingredients": ["キャベツ", "合いびき肉", "玉ねぎ", "パン粉", "牛乳", "卵"]
     },
     {
         "id": 5,
@@ -33,7 +33,139 @@ recipes = [
         "id": 6,
         "name": "ピーマンの肉詰め",
         "category": "主菜",
-        "ingredients": ["ピーマン", "ひき肉", "玉ねぎ"]
+        "ingredients": ["ピーマン", "合いびき肉", "玉ねぎ"]
+    },
+    {
+        "id": 7,
+        "name": "鶏の照り焼き",
+        "category": "主菜",
+        "ingredients": ["鶏もも肉"]
+    },
+    {
+        "id": 8,
+        "name": "唐揚げ",
+        "category": "主菜",
+        "ingredients": ["鶏もも肉"]
+    },
+    {
+        "id": 9,
+        "name": "とんかつ",
+        "category": "主菜",
+        "ingredients": ["豚ロース", "卵", "パン粉"]
+    },
+    {
+        "id": 10,
+        "name": "チキン南蛮",
+        "category": "主菜",
+        "ingredients": ["鶏むね肉", "卵"]
+    },
+    {
+        "id": 11,
+        "name": "ぶりの照り焼き",
+        "category": "主菜",
+        "ingredients": ["ぶり"]
+    },
+    {
+        "id": 12,
+        "name": "鮭のムニエル",
+        "category": "主菜",
+        "ingredients": ["鮭", "バター"]
+    },
+    {
+        "id": 13,
+        "name": "さばの味噌煮",
+        "category": "主菜",
+        "ingredients": ["さば", "味噌", "しょうが"]
+    },
+    {
+        "id": 14,
+        "name": "八宝菜",
+        "category": "主菜",
+        "ingredients": ["白菜", "豚肉", "にんじん", "しいたけ", "たけのこ"]
+    },
+    {
+        "id": 15,
+        "name": "青椒肉絲",
+        "category": "主菜",
+        "ingredients": ["牛肉", "ピーマン", "たけのこ"]
+    },
+    {
+        "id": 16,
+        "name": "回鍋肉",
+        "category": "主菜",
+        "ingredients": ["豚肉", "キャベツ", "ピーマン"]
+    },
+    {
+        "id": 17,
+        "name": "酢豚",
+        "category": "主菜",
+        "ingredients": ["豚肉", "玉ねぎ", "にんじん", "ピーマン"]
+    },
+    {
+        "id": 18,
+        "name": "エビチリ",
+        "category": "主菜",
+        "ingredients": ["えび", "長ねぎ"]
+    },
+    {
+        "id": 19,
+        "name": "チンジャオロース",
+        "category": "主菜",
+        "ingredients": ["牛肉", "ピーマン", "たけのこ"]
+    },
+    {
+        "id": 20,
+        "name": "オムライス",
+        "category": "主菜",
+        "ingredients": ["卵", "ご飯", "鶏肉", "玉ねぎ", "ケチャップ"]
+    },
+    {
+        "id": 21,
+        "name": "グラタン",
+        "category": "主菜",
+        "ingredients": ["鶏肉", "玉ねぎ", "牛乳", "チーズ"]
+    },
+    {
+        "id": 22,
+        "name": "ラザニア",
+        "category": "主菜",
+        "ingredients": ["ひき肉", "チーズ", "トマト"]
+    },
+    {
+        "id": 23,
+        "name": "ビーフシチュー",
+        "category": "主菜",
+        "ingredients": ["牛肉", "玉ねぎ", "にんじん", "じゃがいも"]
+    },
+    {
+        "id": 24,
+        "name": "チキンソテー",
+        "category": "主菜",
+        "ingredients": ["鶏もも肉"]
+    },
+    {
+        "id": 25,
+        "name": "タンドリーチキン",
+        "category": "主菜",
+        "ingredients": ["鶏もも肉", "ヨーグルト"]
+    },
+    {
+        "id": 26,
+        "name": "ヤンニョムチキン",
+        "category": "主菜",
+        "ingredients": ["鶏もも肉"]
+    },
+    {
+        "id": 27,
+        "name": "プルコギ",
+        "category": "主菜",
+        "ingredients": ["牛肉", "玉ねぎ", "にんじん"]
+    },
+    {
+        "id": 28,
+        "name": "ゴーヤチャンプルー",
+        "category": "主菜",
+        "ingredients": ["ゴーヤ", "豚肉", "豆腐", "卵"]
     },
     {
         "id": 101,
@@ -94,5 +226,65 @@ recipes = [
         "name": "もやしナムル",
         "category": "副菜",
         "ingredients": ["もやし", "ごま"]
+    },
+    {
+        "id": 201,
+        "name": "味噌汁",
+        "category": "汁物",
+        "ingredients": ["豆腐", "わかめ", "長ねぎ", "味噌"]
+    },
+    {
+        "id": 202,
+        "name": "豚汁",
+        "category": "汁物",
+        "ingredients": ["豚肉", "大根", "にんじん", "ごぼう", "こんにゃく", "長ねぎ", "味噌"]
+    },
+    {
+        "id": 203,
+        "name": "けんちん汁",
+        "category": "汁物",
+        "ingredients": ["大根", "にんじん", "ごぼう", "豆腐", "こんにゃく", "長ねぎ"]
+    },
+    {
+        "id": 204,
+        "name": "かきたま汁",
+        "category": "汁物",
+        "ingredients": ["卵", "長ねぎ", "片栗粉"]
+    },
+    {
+        "id": 205,
+        "name": "わかめスープ",
+        "category": "汁物",
+        "ingredients": ["わかめ", "長ねぎ", "ごま"]
+    },
+    {
+        "id": 206,
+        "name": "コーンスープ",
+        "category": "汁物",
+        "ingredients": ["コーン", "牛乳", "玉ねぎ", "バター"]
+    },
+    {
+        "id": 207,
+        "name": "ミネストローネ",
+        "category": "汁物",
+        "ingredients": ["トマト", "玉ねぎ", "にんじん", "じゃがいも", "キャベツ"]
+    },
+    {
+        "id": 208,
+        "name": "オニオンスープ",
+        "category": "汁物",
+        "ingredients": ["玉ねぎ", "バター", "コンソメ"]
+    },
+    {
+        "id": 209,
+        "name": "中華スープ",
+        "category": "汁物",
+        "ingredients": ["卵", "長ねぎ", "ごま", "鶏がらスープの素"]
+    },
+    {
+        "id": 210,
+        "name": "クラムチャウダー",
+        "category": "汁物",
+        "ingredients": ["あさり", "じゃがいも", "玉ねぎ", "にんじん", "牛乳", "バター"]
     }
 ]
